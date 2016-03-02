30137bc7869f97da5ed05eb4100d17b54c44a71c
Abishek Reddy <abishekwdaru@gmail.com>
2016-03-02 16:06:32 -0500

    commit with random number generator

--- src/KeepGuessing.java
+++ src/KeepGuessing.java
@@ -6,15 +6,23 @@ public class KeepGuessing {
 		Scanner keyboard = new Scanner(System.in);
 		int secretNumber, guess = 0;//guess is initialized to 0
 		Random rand = new Random();
-		secretNumber = 1 + rand.nextInt(10);
+		secretNumber = 1 + rand.nextInt(100);
 		
-		System.out.println("I'm thinking of a number between 1 and 1000");
+		System.out.println("I'm thinking of a number between 1 and 100");
 		System.out.print("Enter the number:");
 		guess = keyboard.nextInt();
 		
 		while ( guess != secretNumber && guess != 5)
 		{
 			System.out.println("\nYou are wrong. Try again.");
+			if(guess < secretNumber)
+			{
+				System.out.println("Guess Higher");
+			}
+			else
+			{
+				System.out.println("Guess Lower");
+			}
 			System.out.println("Enter the number: ");
 			guess = keyboard.nextInt();
 		}
